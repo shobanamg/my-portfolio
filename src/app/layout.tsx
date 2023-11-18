66ef85bf249cf99e10a909fa66b40fbabf737b47
Shobana Mathiarul <mathiarulshobana@gmail.com>
2023-11-18 02:04:53 +0100

    feat - designed header + button component + dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 
+import './globals.css';
+import Header from '@/components/layout/header';
+import { Providers } from '@/lib/providers';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -12,9 +16,16 @@ export default function RootLayout({
                                    }: {
   children: React.ReactNode;
 }) {
-  return (
-      <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-      </html>
-  );
+    return (
+        <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+        <body
+            className={`${inter.className} bg-gray-0 text-gray-600 antialiased`}
+        >
+        <Providers>
+            <Header />
+            <main className="flex min-h-screen w-full flex-col">{children}</main>
+        </Providers>
+        </body>
+        </html>
+    );
 }
