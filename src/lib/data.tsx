e6a2ab195a717107e913e7e0c1545e43723cff63
Shobana Mathiarul <mathiarulshobana@gmail.com>
2023-11-18 02:41:25 +0100

    feat - mobile menu

---
+++ src/lib/data.tsx
@@ -0,0 +1,14 @@
+export const NAV_LINKS = [
+    {
+        label: 'About',
+        href: '#about',
+    },
+    {
+        label: 'Work',
+        href: '#work',
+    },
+    {
+        label: 'Contact',
+        href: '#contact',
+    },
+];
