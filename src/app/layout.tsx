7c2fc1f6b88c54baeb734f4c697645241eedf4d6
Shobana Mathiarul <mathiarulshobana@gmail.com>
2023-11-26 03:58:21 +0100

    feat - added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,46 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import Script from "next/script";
+import { Inter } from "next/font/google";
 
-import './globals.css';
-import Header from '@/components/layout/header';
-import { Providers } from '@/lib/providers';
+import "./globals.css";
+import Header from "@/components/layout/header";
+import { Providers } from "@/lib/providers";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Shobana Mathiarul | Full Stack Developer From Copenhagen, Denmark',
+  title: "Shobana Mathiarul | Full Stack Developer From Copenhagen, Denmark",
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: {
+  children,
+}: {
   children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
-        <body
-            className={`${inter.className} bg-gray-0 text-gray-600 antialiased`}
-        >
+  return (
+    <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BWPBE3C0MV"
+        ></Script>
+        <Script id="google-anayltics-script">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-BWPBE3C0MV');
+          `}
+        </Script>
+      </head>
+      <body
+        className={`${inter.className} bg-gray-0 text-gray-600 antialiased`}
+      >
         <Providers>
-            <Header />
-            <main className="flex min-h-screen w-full flex-col">{children}</main>
+          <Header />
+          <main className="flex min-h-screen w-full flex-col">{children}</main>
         </Providers>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
